34a2dc03c5ca5e8c349950a0d14c8496bafe793f
ceasinugo <ceasinugo@wustl.edu>
2024-07-21 12:12:47 -0500

    Class 10 Updates

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,9 +1,18 @@
 function gradeLabs(labs) {
+
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+
+    try{
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    }catch{
+      let result = 'Error thrown';
+      console.log(`${lab.student} code did not work: ${result}`);
+    }
   }
+
+  
 }
 
 let studentLabs = [
@@ -21,4 +30,32 @@ let studentLabs = [
   }
 ];
 
+
+
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+
+console.log();
 gradeLabs(studentLabs);
+console.log();
+gradeLabs(studentLabs2);
