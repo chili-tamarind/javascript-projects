476e96b87d7ddee7caff48c6d39efc850942f124
ceasinugo <ceasinugo@wustl.edu>
2024-07-30 12:19:44 -0500

    Class 13 Updates

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,8 +2,31 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
-
+    
     // Put your code for the exercises here.
+    
+    /* When the Take off button is clicked, the text The shuttle is on the ground changes to Houston, we have liftoff!. The Take off button has an id="liftoffButton" attribute.*/
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    /* When the user’s cursor goes over the Abort Mission button, the button’s background turns red. The Abort Mission button has id="abortMission".*/
+
+    missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", function() {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    /* When the user clicks the Abort Mission button, make a abort_message window that says Are you sure you want to abort the mission?. If the user confirms that they want to abort, the text changes to Mission aborted! Space shuttle returning home. */
+
+    missionAbort.addEventListener("click", function(){
+        const abort_message = window.confirm("Are you sure you want to abort the mission?");
+            if (abort_message === true){
+                paragraph.innerHTML = "Mission aborted! Space shuttle returning home"
+            }
+    })
     
 }
 
--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,142 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+/* Use the window load event to ensure all elements have loaded before attaching event handlers.
+    window.addEventListener("load", function () {
+    }
+*/
+
+function init () { 
+
+    // Buttons
+    const takeoff = document.getElementById("takeoff"); 
+    const landing = document.getElementById("landing"); 
+    const missionAbort = document.getElementById("missionAbort"); 
+    
+    // Values
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const flightStatus = document.getElementById("flightStatus");
+    
+
+    // Image
+    const rocket = document.getElementById("rocket");
+   // const launchBackground = parseInt(window.getComputedStyle(shuttleBackground). getPropertyValue('width')); // to get number without the pixels
+
+    // Positions 
+    let rocketAltitude = 0;
+    let rocketImageX = 0;
+    let rocketImageY = 0;
+    
+    
+        // When the “Take off” button is clicked
+        takeoff.addEventListener('click', event => {
+            
+            // A window confirm should crosscheck
+            const takeoff_message = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+            // If the shuttle is ready for liftoff
+            if (takeoff_message === true){
+                flightStatus.innerHTML = 'Shuttle in flight.';
+                shuttleBackground.style.backgroundColor = "blue";                
+                
+                rocketAltitude = 10000;
+                spaceShuttleHeight.innerHTML = rocketAltitude;    
+                
+                rocketImageY = 10;
+                rocket.style.marginBottom =  rocketImageY + 'px';
+            }
+        });      
+        
+        // When the “Land” button is clicked, the following should happen:
+        landing.addEventListener('click', event => {
+            
+            // A window confirm should crosscheck
+            const land_message = window.alert("The shuttle is landing. Landing gear engaged.");
+                flightStatus.innerHTML = 'The shuttle has landed.';
+                rocketReset();       
+        });
+
+        // When the “Abort Mission” button is clicked, the following should happen:
+        missionAbort.addEventListener('click', event => {
+            
+            // A window confirm should crosscheck
+            const missionAbort_message = window.confirm("Confirm that you want to abort the mission.");
+
+            // If the land_message is ready for liftoff
+            if (missionAbort_message === true){
+                flightStatus.innerHTML = 'Mission aborted.';
+                rocketReset();                
+            }
+        });
+
+        // Carrie - Use event delegation for direction buttons */
+
+        document.addEventListener("click", function(event) {
+
+            let launchBackgroundWidth = parseInt(window.getComputedStyle(shuttleBackground). getPropertyValue('width')); // to get number without the pixels
+            
+            // When the “Right” & “Left” buttons are clicked, the rocket rocket should move 10 px in the direction of the button that was clicked.
+            if (event.target.id == "left" && (0.5*rocketImageX > 0)) {
+                rocketImageX -= 10;                     
+                rocket.style.marginLeft =  rocketImageX + 'px'; 
+                // number plus string concatenate         
+            } 
+            if (event.target.id == "right" && (rocketImageX < (launchBackgroundWidth-75))) {
+                rocketImageX += 10;
+                rocket.style.marginLeft =  rocketImageX + 'px'; 
+            }
+
+            // When the “Up” & “Down” buttons are clicked, the shuttle height should increase or decrease by 10,000 miles. 
+            if (event.target.id == "up" && rocketAltitude < 250000) { // 25 clicks to the top
+                rocketImageY += 10;
+                rocket.style.marginBottom =  rocketImageY + 'px';
+
+                rocketAltitude += 10000;
+                spaceShuttleHeight.innerHTML = rocketAltitude; 
+            }
+            if (event.target.id == "down" && rocketAltitude > 0) {
+                rocketImageY -= 10;
+                rocket.style.marginBottom =  rocketImageY + 'px';
+
+                rocketAltitude -= 10000; 
+                spaceShuttleHeight.innerHTML = rocketAltitude;
+            }                
+        });
+
+        /* BONUS
+
+        Keep the rocket from flying off of the background.
+        
+        Return the rocket to its original position on the background when it has been landed or the mission was aborted.  */
+
+        // Helper/utility function 
+
+        function rocketReset() {
+
+            let launchBackgroundWidth = parseInt(window.getComputedStyle(shuttleBackground). getPropertyValue('width')); // to get number without the pixels
+
+            shuttleBackground.style.backgroundColor = "green";              
+            
+            rocketAltitude = 0;    
+            spaceShuttleHeight.innerHTML = rocketAltitude;        
+            
+            rocketImageX = launchBackgroundWidth/2 -75;
+            rocket.style.marginLeft = rocketImageX + 'px';
+            
+            rocketImageY = 0;
+            rocket.style.marginBottom = rocketImageY + 'px';
+        }
+    
+        function rocketImagePositionY() {
+            let launchBackgroundHeight = parseInt(window.getComputedStyle(shuttleBackground). getPropertyValue('height')); // to get number without the pixels
+            
+            //rocketAltitude = Number(y);
+            // spaceShuttleHeight.innerHTML = rocketAltitude;      
+
+            //rocketImageY = launchBackgroundHeight*rocketAltitude*;
+            //rocket.style.marginBottom =  rocketImageY + 'px';
+        }
+}
+
+window.addEventListener("load", init);
